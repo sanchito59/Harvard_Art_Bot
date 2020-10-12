c3b878eb271c20389629d947928047e314b1cac6
Chris Sanchez <c.sanch7@gmail.com>
2020-10-11 22:54:38 -0700

    update config

--- negatives.js
+++ negatives.js
@@ -6,7 +6,7 @@ const request = require('request');
 const config = {
   consumer_key: process.env.NEGATIVES_CONSUMER_KEY,
   consumer_secret: process.env.NEGATIVES_CONSUMER_SECRET,
-  access_token: process.env.NEGATIVES_ACCESS_TOKEN,
+  access_token_key: process.env.NEGATIVES_ACCESS_TOKEN,
   access_token_secret: process.env.NEGATIVES_ACCESS_TOKEN_SECRET
 };
 
